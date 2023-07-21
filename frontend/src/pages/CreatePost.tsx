0a9b1b35abe3dc169d71654e9278013375d5e465
holladortun <holladortun.omotoso@gmail.com>
2023-07-21 01:40:52 +0100

    cors policy added

--- frontend/src/pages/CreatePost.tsx
+++ frontend/src/pages/CreatePost.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import { useState } from 'react';
 
+import sampleImg from '../assets/sampleimg.jpg';
 import useCreatePost from '../hooks/useCreatePost';
 
 const CreatePost = () => {
@@ -16,13 +17,14 @@ const CreatePost = () => {
   };
 
   return (
-    <div className='max-w-[1440px] mx-auto px-[50px] container flex justify-center pt-20'>
+    <div className='max-w-[1440px] mx-auto px-[50px] container flex justify-center py-20'>
       <div className=' w-[550px] flex flex-col items-center '>
         <h2 className='text-3xl font-bold text-primary'>Publish a Post</h2>
         <form
           className='flex flex-col w-full gap-8 p-5 mt-10 shadow-md rounded-xl'
           onSubmit={handleCreatePost}
         >
+          <img src={sampleImg} alt='' className='rounded-md' />
           <div className='flex flex-col gap-1'>
             <label className='font-medium text-[18px]'>Title</label>
             <input
@@ -42,9 +44,14 @@ const CreatePost = () => {
                 setContent(e.target.value);
               }}
               className='px-2 py-2 border-2 rounded-md text-[15px]'
+              rows={10}
             ></textarea>
           </div>
-          <input type='submit' value='Create Post' />
+          <input
+            type='submit'
+            value='Create Post'
+            className='py-2 text-white rounded-md cursor-pointer bg-primary'
+          />
         </form>
       </div>
     </div>
